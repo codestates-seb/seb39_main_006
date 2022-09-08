d4712047c6d7ac76c63d02a54947911f43eab25f
jyj2187 <jyjgom@gmail.com>
2022-09-08 17:48:41 +0900

    first init

---
+++ server/src/main/java/com/codestates/seb006main/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.seb006main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
