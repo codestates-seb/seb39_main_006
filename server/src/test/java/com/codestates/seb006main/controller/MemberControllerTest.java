c1923d4dee3899fb73a54df566079f33b23628ec
ki-hong <rlghd153@gmail.com>
2022-09-17 18:40:22 +0900

    [TEST] MemberControllerTest 회원 정보 수정 Test 완료

--- server/src/test/java/com/codestates/seb006main/controller/MemberControllerTest.java
+++ server/src/test/java/com/codestates/seb006main/controller/MemberControllerTest.java
@@ -67,7 +67,7 @@ public class MemberControllerTest {
 
         //when
         ResultActions actions = mockMvc.perform(
-                post("/api/members")
+                patch("/api/members")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content)
@@ -86,6 +86,71 @@ public class MemberControllerTest {
                                         fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
                                         fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
                                         fieldWithPath("display_name").type(JsonFieldType.STRING).description("닉네임")
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("아이디"),
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("display_name").type(JsonFieldType.STRING).description("닉네임"),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("전화번호"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("소개"),
+                                        fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("회원 상태"),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("계정 생성일"),
+                                        fieldWithPath("modifiedAt").type(JsonFieldType.STRING).description("계정 수정일").optional(),
+                                        fieldWithPath("profileImage").type(JsonFieldType.STRING).description("프로필 이미지"),
+                                        fieldWithPath("role").type(JsonFieldType.STRING).description("회원권한")
+                                )
+                        )
+                ));
+    }
+    @Test
+    public void patchMemberTest() throws Exception {
+        //given
+        long memberId = 1L;
+        MemberDto.Patch patch = new MemberDto.Patch("kkhong", "123456", "01012349876", "","");
+        String content = gson.toJson(patch);
+        System.out.println(content);
+        MemberDto.Response response = MemberDto.Response.builder()
+                .memberId(1L)
+                .email("rlghd@gmail.com")
+                .display_name("kkhong")
+                .phone("01012349876").content("")
+                .memberStatus(Member.MemberStatus.ACTIVE)
+                .createdAt(LocalDateTime.now())
+                .modifiedAt(LocalDateTime.now())
+                .profileImage("")
+                .role(Member.Role.ROLE_MEMBER)
+                .build();
+
+        //Mock
+        given(memberService.modifyMember(Mockito.any(MemberDto.Patch.class),Mockito.anyLong())).willReturn(response);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                patch("/api/members/{member-id}", memberId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.display_name").value(patch.getDisplay_name()))
+                .andExpect(jsonPath("$.phone").value(patch.getPhone()))
+                .andExpect(jsonPath("$.content").value(patch.getContent()))
+                .andExpect(jsonPath("$.profileImage").value(patch.getProfileImage()))
+                .andDo(document(
+                        "post-member",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("display_name").type(JsonFieldType.STRING).description("닉네임"),
+                                        fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대전화 번호"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("자기소개"),
+                                        fieldWithPath("profileImage").type(JsonFieldType.STRING).description("이미지 url")
                                 )
                         ),
                         responseFields(
