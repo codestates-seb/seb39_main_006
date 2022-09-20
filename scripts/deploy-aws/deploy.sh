3500053ed137187b3930c878133c544ac9113a33
jyj2187 <jyjgom@gmail.com>
2022-09-20 19:35:32 +0900

    [Edit] deploy.sh 수정

--- scripts/deploy-aws/deploy.sh
+++ scripts/deploy-aws/deploy.sh
@@ -10,8 +10,11 @@ echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
 DEPLOY_PATH=/home/ubuntu/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+#echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+#CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+echo "> 8080 PORT 에서 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(lsof -ti tcp:8080)
 
 if [ -z $CURRENT_PID ]
 then
@@ -25,4 +28,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
