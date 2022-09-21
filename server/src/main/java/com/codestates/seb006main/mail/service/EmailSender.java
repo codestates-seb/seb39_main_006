a7beb919181ac5ebd7b18cf4dec4b5a187702827
ki-hong <rlghd153@gmail.com>
2022-09-22 03:27:48 +0900

    [Edit] EmailSender 불필요한 변수 삭제

--- server/src/main/java/com/codestates/seb006main/mail/service/EmailSender.java
+++ server/src/main/java/com/codestates/seb006main/mail/service/EmailSender.java
@@ -19,8 +19,6 @@ public class EmailSender {
 
     private String subject = "[이메일 인증] 홈페이지에 인증번호를 입력해주세요.";
 
-    private String massageSuffix = "";
-
     public void sendEmail(String to, String code) throws GeneralSecurityException, UnsupportedEncodingException {
         try {
             String context = "인증번호는 ["+code+"]입니다.";
