2c56f50c8f051950140edba564eda187b698f776
Lmoti <tv13579@naver.com>
2022-09-27 16:38:59 +0900

    [Edit] toast 코드 누락 해결 및 수정

--- client/src/Pages/PostPage/NewPost.js
+++ client/src/Pages/PostPage/NewPost.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
 
 const NewPost = () => {
   return (
@@ -10,7 +11,7 @@ const NewPost = () => {
         placeholder="내용을 입력해주세요."
         previewStyle="vertical" // 미리보기 스타일 지정
         height="300px" // 에디터 창 높이
-        initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+        initialEditType="markdown" // 초기 입력모드 설정(디폴트 markdown)
         toolbarItems={[
           // 툴바 옵션 설정
           ["heading", "bold", "italic", "strike"],
