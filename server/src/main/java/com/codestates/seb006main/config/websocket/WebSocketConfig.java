0ddd75e6ca9cbe620de1b1d98a59dd04bd887afb
jyj2187 <jyjgom@gmail.com>
2022-10-04 16:43:13 +0900

    [Edit] 웹소켓 CORS 에러 핸들링

--- server/src/main/java/com/codestates/seb006main/config/websocket/WebSocketConfig.java
+++ server/src/main/java/com/codestates/seb006main/config/websocket/WebSocketConfig.java
@@ -21,8 +21,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:3000").setAllowedOriginPatterns("*");
-//        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:3000").setAllowedOriginPatterns("*").withSockJS();
         registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:3000", "https://www.hitch-hiker.kr").setAllowedOriginPatterns("*");
         registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:3000", "https://www.hitch-hiker.kr").setAllowedOriginPatterns("*").withSockJS();
     }
