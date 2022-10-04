da0ef4f4ef31288730a30ff2213a991fd1065f78
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-10-04 17:26:17 +0900

    [Matching : grid,css]

--- client/src/pages/postpage/Matching.js
+++ client/src/pages/postpage/Matching.js
@@ -42,33 +42,90 @@ const Matching = () => {
   };
 
   return (
-    <div>
-      <h1>매칭 게시글 : {matchData.postTitle}</h1>
-      <div>매칭 신청자 : {matchData.memberName}</div>
-      <div>매칭 내용</div>
-      <MatchBody>{matchData.body}</MatchBody>
-      <span>
-        <Button
-          onClick={() => {
-            acceptHandler();
-          }}
-        >
-          수락
-        </Button>
-        <Button
-          onClick={() => {
-            refuseHandler();
-          }}
-        >
-          거절
-        </Button>
-      </span>
-    </div>
+    <PageContainer>
+      <Container>
+        <ContentWrap>
+          <p>매칭 게시글 :</p>
+          <h1 className="title"> {matchData.postTitle}</h1>
+          <p>매칭 신청자 : </p>
+          <div className="person">{matchData.memberName}</div>
+          <p>매칭 내용 :</p>
+          <div className="contents"></div>
+          <MatchBody>{matchData.body}</MatchBody>
+          <span>
+            <Button
+              onClick={() => {
+                acceptHandler();
+              }}
+            >
+              수락
+            </Button>
+            <Button
+              onClick={() => {
+                refuseHandler();
+              }}
+            >
+              거절
+            </Button>
+          </span>
+        </ContentWrap>
+      </Container>
+    </PageContainer>
   );
 };
 
 export default Matching;
+const PageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5px 0 5px;
 
+  @media screen and (max-width: 1000px) {
+    padding: 30px 25px 30px 25px;
+    height: 700px;
+  }
+`;
+const Container = styled.div`
+  margin: 150px 0 250px 0;
+  padding: 40px 50px 40px 50px;
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  width: 100%;
+  height: 900px;
+  background-color: #f5f5dc;
+
+  box-shadow: 0px 0px 11px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  font-family: Roboto;
+  box-sizing: border-box;
+  @media screen and (max-width: 1000px) {
+    padding: 30px 25px 30px 25px;
+    height: 455px;
+  }
+`;
+const ContentWrap = styled.div`
+  p {
+    font-size: 1rem;
+  }
+  h1 {
+    font-size: 2.6rem;
+    font-weight: 700;
+    color: #666;
+  }
+  .person {
+    font-size: 2rem;
+    color: #425049;
+    font-weight: 600;
+  }
+  .contents {
+    font-size: 1.5rem;
+    color: #425049;
+    font-weight: 400;
+  }
+`;
 const MatchBody = styled.div`
   background-color: #d5eaf1;
   width: 70%;
