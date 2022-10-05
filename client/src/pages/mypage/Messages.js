23cd6a568f1de7c831dbb45891fc357298b5a8a7
Lmoti <tv13579@naver.com>
2022-10-05 19:38:28 +0900

    [Add] 새 알람 전체 읽음, 전체 알람들 나열 및 읽었는지 상태 확인

--- client/src/pages/mypage/Messages.js
+++ client/src/pages/mypage/Messages.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SideBar from "../../components/SideBar";
 import H1 from "../../components/ui/H1";
 const Messages = () => {
   const [messages, setMessages] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios(`${process.env.REACT_APP_URL}/api/messages`, {
@@ -11,12 +13,26 @@ const Messages = () => {
         access_hh: sessionStorage.getItem("AccessToken"),
         refresh_hh: sessionStorage.getItem("RefreshToken"),
       },
-    }).then((res) => console.log(res.data.data));
-  });
+    }).then((res) => {
+      setMessages(res.data.data);
+    });
+  }, []);
   return (
     <div>
       <SideBar />
       <H1>알림들</H1>
+      {messages.map((el, idx) => (
+        <div key={idx}>
+          <span
+            onClick={() => {
+              navigate(`/${el.postId}`);
+            }}
+          >
+            {el.body}
+          </span>
+          <span> {el.messageStatus === "READ" ? "읽음" : "안읽음"}</span>
+        </div>
+      ))}
     </div>
   );
 };
