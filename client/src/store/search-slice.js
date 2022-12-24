60fbeb60abc3e20e9d60e815263f0ea450b91663
jyj2187 <jyjgom@gmail.com>
2022-12-25 01:41:59 +0900

    [Fix] 검색 기능 문자열 예외 처리

--- client/src/store/search-slice.js
+++ client/src/store/search-slice.js
@@ -1,38 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialSearchState = {
-  title: "",
-  body: "",
-  location: "",
-  startDate: "",
-  endDate: "",
+	title: "",
+	body: "",
+	location: "",
+	startDate: "",
+	endDate: "",
+};
+
+const checkInput = (input) => {
+	const regExp = /.*[\\`[\]{}].*/gi;
+	if (input.matches(regExp)) {
+		return input.replaceAll(regExp, "");
+	}
+	return input;
 };
 
 const searchSlice = createSlice({
-  name: "search",
-  initialState: initialSearchState,
-  reducers: {
-    setTitle(state, action) {
-      const value = action.payload;
-      state.title = value;
-    },
-    setBody(state, action) {
-      const value = action.payload;
-      state.body = value;
-    },
-    setLocation(state, action) {
-      const value = action.payload;
-      state.location = value;
-    },
-    setStartDate(state, action) {
-      const value = action.payload;
-      state.startDate = value;
-    },
-    setEndDate(state, action) {
-      const value = action.payload;
-      state.endDate = value;
-    },
-  },
+	name: "search",
+	initialState: initialSearchState,
+	reducers: {
+		setTitle(state, action) {
+			const value = action.payload;
+			state.title = checkInput(value);
+		},
+		setBody(state, action) {
+			const value = action.payload;
+			state.body = checkInput(value);
+		},
+		setLocation(state, action) {
+			const value = action.payload;
+			state.location = checkInput(value);
+		},
+		setStartDate(state, action) {
+			const value = action.payload;
+			state.startDate = value;
+		},
+		setEndDate(state, action) {
+			const value = action.payload;
+			state.endDate = value;
+		},
+	},
 });
 
 export const searchActions = searchSlice.actions;
